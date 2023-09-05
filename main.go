51e26a0ad86587fc38e2d3660e9320fec8ee0443
Levi Pare <levipare@gmail.com>
2023-09-05 19:40:12 -0400

    working on display features

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"os"
 	"strconv"
+
+	"github.com/gdamore/tcell/v2"
 
 	"github.com/levipare/go-todo-list/todo"
 )
@@ -30,11 +31,11 @@ func readListFromFile(filename string) todo.TodoList {
 		todoList.AddItem(todo.NewItem(item[0], isCompleted))
 	}
 
-	return todoList;
+	return todoList
 }
 
-func writeListToFile(filename string, lst todo.TodoList)  {
-	file, err := os.Open(filename)
+func writeListToFile(filename string, lst todo.TodoList) {
+	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -42,16 +43,97 @@ func writeListToFile(filename string, lst todo.TodoList)  {
 
 	writer := csv.NewWriter(file)
 	for _, item := range lst.Items {
-		err := writer.Write([]string{item.Description, strconv.FormatBool(item.Completed)})
+		record := []string{item.Description, strconv.FormatBool(item.Completed)}
+		err := writer.Write(record)
 		if err != nil {
 			panic(err)
 		}
 	}
+	writer.Flush()
 }
 
+var todoList todo.TodoList
+var selectedItem = 0
+
 func main() {
-	todoList := readListFromFile("items.csv")
-	todoList.AddItem(todo.NewItem("Make text editor", false))
-	fmt.Println(todoList)
+	todoList = readListFromFile("items.csv")
+	render()
 	writeListToFile("items.csv", todoList)
+}
+
+func render() {
+	s, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	if err = s.Init(); err != nil {
+		panic(err)
+	}
+	defer s.Fini()
+
+	s.SetStyle(tcell.StyleDefault)
+	s.Clear()
+
+	// Blocking input loop
+loop:
+	for {
+		drawItems(s, todoList)
+		s.Show()
+
+		switch ev := s.PollEvent().(type) {
+		case *tcell.EventKey:
+			if ev.Key() == 256 {
+				switch ev.Rune() {
+				case ' ':
+					if selectedItem < len(todoList.Items) {
+						todoList.GetItem(selectedItem).Completed = !todoList.GetItem(selectedItem).Completed
+					}
+				}
+			} else {
+				switch ev.Key() {
+				case tcell.KeyEscape:
+					break loop
+				case tcell.KeyDown, tcell.KeyTab:
+					if selectedItem < len(todoList.Items)-1 {
+						selectedItem++
+					}
+				case tcell.KeyUp, tcell.KeyBacktab:
+					if selectedItem > 0 {
+						selectedItem--
+					}
+				case tcell.KeyEnter, ' ':
+					if selectedItem < len(todoList.Items) {
+						todoList.GetItem(selectedItem).Completed = !todoList.GetItem(selectedItem).Completed
+					}
+				}
+			}
+
+		}
+
+	}
+}
+
+func drawItems(s tcell.Screen, lst todo.TodoList) {
+	for i, item := range lst.Items {
+		style := tcell.StyleDefault
+		if item.Completed {
+			style = style.Dim(true)
+		}
+
+		if i == selectedItem {
+			style = style.Foreground(tcell.ColorDeepPink)
+		}
+
+		symbol := ' '
+		if item.Completed {
+			symbol = 'X'
+		}
+
+		s.SetContent(0, i, '[', nil, style)
+		s.SetContent(1, i, symbol, nil, style)
+		s.SetContent(2, i, ']', nil, style)
+		for j, c := range item.Description {
+			s.SetContent(j+4, i, c, nil, style)
+		}
+	}
 }
