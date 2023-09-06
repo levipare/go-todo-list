dfbb9fda3627937d74eae4fc686da47a70b34216
Levi Pare <levipare@gmail.com>
2023-09-05 22:28:35 -0400

    text input and user inut handling

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func writeListToFile(filename string, lst todo.TodoList) {
 
 var todoList todo.TodoList
 var selectedItem = 0
+var textInput = ""
 
 func main() {
 	todoList = readListFromFile("items.csv")
@@ -72,40 +73,60 @@ func render() {
 	defer s.Fini()
 
 	s.SetStyle(tcell.StyleDefault)
-	s.Clear()
 
 	// Blocking input loop
 loop:
 	for {
-		drawItems(s, todoList)
+		s.Clear()
+		s.HideCursor()
+
+		drawItems(s)
+		drawItemInput(s)
+		drawClearKeyMap(s)
+
 		s.Show()
 
 		switch ev := s.PollEvent().(type) {
 		case *tcell.EventKey:
-			if ev.Key() == 256 {
+			if selectedItem == len(todoList.Items) {
+				if ev.Key() == 256 {
+					textInput = textInput + string(ev.Rune())
+				}
+
+			} else if ev.Key() == 256 {
 				switch ev.Rune() {
 				case ' ':
 					if selectedItem < len(todoList.Items) {
 						todoList.GetItem(selectedItem).Completed = !todoList.GetItem(selectedItem).Completed
 					}
 				}
-			} else {
-				switch ev.Key() {
-				case tcell.KeyEscape:
-					break loop
-				case tcell.KeyDown, tcell.KeyTab:
-					if selectedItem < len(todoList.Items)-1 {
-						selectedItem++
-					}
-				case tcell.KeyUp, tcell.KeyBacktab:
-					if selectedItem > 0 {
-						selectedItem--
-					}
-				case tcell.KeyEnter, ' ':
-					if selectedItem < len(todoList.Items) {
-						todoList.GetItem(selectedItem).Completed = !todoList.GetItem(selectedItem).Completed
-					}
+			}
+
+			switch ev.Key() {
+			case tcell.KeyEscape:
+				break loop
+			case tcell.KeyDown, tcell.KeyTab:
+				if selectedItem < len(todoList.Items) {
+					selectedItem++
 				}
+			case tcell.KeyUp, tcell.KeyBacktab:
+				if selectedItem > 0 {
+					selectedItem--
+				}
+			case tcell.KeyEnter, ' ':
+				if selectedItem < len(todoList.Items) {
+					todoList.GetItem(selectedItem).Completed = !todoList.GetItem(selectedItem).Completed
+				} else if selectedItem == len(todoList.Items) && textInput != "" {
+					todoList.AddItem(todo.NewItem(textInput, false))
+					textInput = ""
+					selectedItem = len(todoList.Items)
+				}
+			case tcell.KeyBackspace, tcell.KeyBackspace2:
+				if selectedItem == len(todoList.Items) && len(textInput) > 0 {
+					textInput = textInput[:len(textInput)-1]
+				}
+			case tcell.KeyCtrlX:
+				todoList = todo.NewList()
 			}
 
 		}
@@ -113,15 +134,60 @@ loop:
 	}
 }
 
-func drawItems(s tcell.Screen, lst todo.TodoList) {
-	for i, item := range lst.Items {
+func drawClearKeyMap(s tcell.Screen) {
+	for i, c := range "to clear list -> ctrl+x" {
+		_, h := s.Size()
+		s.SetContent(i, h-1, c, nil, tcell.StyleDefault.Dim(true))
+	}
+}
+
+func drawItemInput(s tcell.Screen) {
+	oX, oY, w, h := 0, 0, 0, 0
+	oY = len(todoList.Items) + 1
+	w = 32
+	h = 3
+
+	style := tcell.StyleDefault
+	if selectedItem == len(todoList.Items) {
+		style = style.Foreground(tcell.ColorIndianRed)
+		s.ShowCursor(oX+1+len(textInput), oY+1)
+	}
+
+	s.SetContent(oX, oY, '╭', nil, style)
+	s.SetContent(oX+w-1, oY, '╮', nil, style)
+	s.SetContent(oX, oY+h-1, '╰', nil, style)
+	s.SetContent(oX+w-1, oY+h-1, '╯', nil, style)
+
+	for x := oX + 1; x < oX+w-1; x++ {
+		s.SetContent(x, oY, '─', nil, style)
+		s.SetContent(x, oY+h-1, '─', nil, style)
+
+		if x-oX-1 < len(textInput) {
+			s.SetContent(x, oY+1, rune(textInput[x-oX-1]), nil, tcell.StyleDefault)
+		}
+	}
+
+	for _, x := range []int{oX, oX + w - 1} {
+		for y := oY + 1; y < oY+h-1; y++ {
+			s.SetContent(x, y, '│', nil, style)
+		}
+	}
+
+	for i, c := range " new todo " {
+		s.SetContent(oX+i+1, oY, c, nil, style)
+	}
+
+}
+
+func drawItems(s tcell.Screen) {
+	for i, item := range todoList.Items {
 		style := tcell.StyleDefault
 		if item.Completed {
 			style = style.Dim(true)
 		}
 
 		if i == selectedItem {
-			style = style.Foreground(tcell.ColorDeepPink)
+			style = style.Foreground(tcell.ColorIndianRed)
 		}
 
 		symbol := ' '
